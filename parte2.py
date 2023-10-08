882216191ba78f6330ffd892712cea2d3f5aa362
Andres <xndres2301@gmail.com>
2023-10-08 15:07:10 -0500

    master

---
+++ parte2.py
@@ -0,0 +1,20 @@
+from datasets import load_dataset
+import numpy as np
+import pandas as pd
+
+dataset = load_dataset("mstz/heart_failure") # Cargar el dataset
+data = dataset["train"]
+
+edades = data["age"]
+edades_pd = np.array(edades)
+promedio_edad = np.mean(edades_pd) # Calcular el promedio de edad directamente desde el arreglo de NumPy
+print(f'Promedio de Edad: {promedio_edad}')
+
+df = pd.DataFrame(data)
+personas_perecidas = df[df["is_dead"] == 1]
+personas_vivas = df[df["is_dead"] != 1]
+
+promedio_edad_perecidas = personas_perecidas["age"].mean()
+promedio_edad_vivas = personas_vivas["age"].mean()
+
+print(df)
