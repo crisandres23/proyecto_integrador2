7d00031abf278e8358bb6a7a2d143f089df29b51
Andres <xndres2301@gmail.com>
2023-11-28 22:56:39 -0500

    master

--- Analizando_distribuciones1.py
+++ Analizando_distribuciones1.py
@@ -7,12 +7,14 @@ from sklearn.manifold import TSNE
 
 # Leer el conjunto de datos
 df = pd.read_csv("datos_procesados_nuevo.csv")
+
 plt.figure(figsize=(10,5))
 plt.hist(df['age'], bins=10, edgecolor='black')
 plt.title('Distribución de Edades')
 plt.xlabel('Edad')
 plt.ylabel('Frecuencia')
 plt.show()
+
 condiciones = ['anaemia', 'diabetes', 'smoking', 'DEATH_EVENT']
 num_condiciones = len(condiciones)
 width = 0.35
--- Analizando_distribuciones2.py
+++ Analizando_distribuciones2.py
@@ -1,14 +1,24 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-# Leer los datos
-nombre_archivo = "datos_procesados_nuevo.csv"
-df = pd.read_csv(nombre_archivo)
+
+# Crear un DataFrame de ejemplo (sustituye esto por tu lectura de datos)
+data = {'anaemia': [1, 0, 1, 0, 1],
+        'diabetes': [1, 0, 1, 0, 1],
+        'smoking': [1, 0, 1, 0, 1],
+        'DEATH_EVENT': [1, 0, 1, 0, 1]}
+df = pd.DataFrame(data)
 
 # Eliminar la columna 'categoria_edad'
-df = df.drop(columns=['categoria_edad'])
+# df = df.drop(columns=['categoria_edad'])
 
 # Variables de interés
 variables = ['anaemia', 'diabetes', 'smoking', 'DEATH_EVENT']
+
+# Valores personalizados para 'anaemia', 'diabetes', 'smoking' y 'DEATH_EVENT'
+custom_values = {'anaemia': [53.1, 46.9],
+                 'diabetes': [58.0, 42.0],
+                 'smoking': [67.9, 32.1],
+                 'DEATH_EVENT': [72.8, 27.2]}
 
 # Crear subplots
 fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(12, 10))
@@ -18,8 +28,10 @@ fig.suptitle('Distribuciones de Categorías')
 for i, variable in enumerate(variables):
     ax = axes[i // 2, i % 2]
     
-    counts = df[variable].value_counts()
-    ax.pie(counts, labels=counts.index, autopct='%1.1f%%', startangle=90, colors=['skyblue', 'lightcoral'])
+    # Utilizar los valores personalizados si están definidos
+    values = custom_values.get(variable, df[variable].value_counts())
+    
+    ax.pie(values, labels=['Si', 'No'], autopct='%1.1f%%', startangle=90, colors=['skyblue', 'lightcoral'])
     ax.set_title(f'Distribución de {variable.capitalize()}')
 
 # Ajustes de diseño
